e80e4c938f355d1faab11d855126b64a768f7a69
Your Name <ludosheikh@gmail.com>
2023-08-20 17:07:38 +0500

    content should be animated wrt bg green div tc

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import { useState } from "react"
-import Main from "./components/main"
-
+import { useState, useRef } from "react";
+import Firstside from "./components/firstside";
+import Secondside from "./components/secondside";
 function App() {
-  const [side, setside] = useState(Boolean)  //true is firstside and false is second side
-  const changeside = ()=>{
-
-    side?setside(false):setside(true)
-  }
+  const bgrotatable = useRef(null);
+  const content = useRef(null);
+  const [side, setside] = useState(true); //true is firstside and false is second side
+  const changeside = () => {
+    side ? setside(false) : setside(true);
+    bgrotatable.current.style.animation = side
+    ? "animationsidechange 3s forwards"
+    : "animationsidechange-reverse 3s forwards";
+    
+    content.current.style.transform = "scale(0.8)"
+  };
   return (
     <>
       <button
         onClick={changeside}
-        className="border border-white px-4 py-2 rounded-full capitalize transition-all duration-500 absolute top-10 right-10 text-white"
+        className="border border-white px-4 py-2 rounded-full capitalize transition-all duration-500 absolute top-10 right-10 z-50 text-white"
       >
         Go to {side ? "Second" : "First"} side
       </button>
-      <Main side={side}/>
+
+      <div
+        ref={bgrotatable}
+        className={`bg-green-600 h-screen w-full fixed top-0 left-0`}
+      ></div>
+      <div ref={content} className="z-20 relativ h-screen w-full transition-all duration-500">{side ? <Firstside></Firstside> : <Secondside></Secondside>}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/maindiv.jsx
@@ -0,0 +1,22 @@
+  // import React from "react";
+  // import Firstside from "./firstside";
+  // import Secondside from "./secondside";
+  // import { useSpring, animated } from 'react-spring';
+
+
+
+  // export default function Main(props) {
+  //   // const props = useSpring({ opacity: 1, transform: 'translateY(0px)' });
+  //   // return <animated.div style={props}>Animated Content</animated.div>;
+  //   return (
+  //     <>
+  //       <div
+  //         id="rotatableDiv"
+  //         className={`bg-green-600 text-white h-screen w-full flex justify-end items-start p-4 origin-center ${props.side?"forwardanimation":"reverseanimation"}`}
+  //       >
+  //         <h1 className="bgadsf">fasdf</h1>
+  //         {props.side ? <Firstside></Firstside> : <Secondside></Secondside>}
+  //       </div>
+  //     </>
+  //   );
+  // }
